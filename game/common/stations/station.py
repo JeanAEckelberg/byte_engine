8f68056cb808b73fcf383916c4fd842b5aae52c2
Gunnar Moody <115500822+MoodyMan04@users.noreply.github.com>
2023-02-27 15:05:49 -0600

    basics of occupied_station

--- game/common/stations/station.py
+++ game/common/stations/station.py
@@ -3,13 +3,14 @@ from game.common.game_object import GameObject
 from game.common.enums import ObjectType
 from game.common.items.item import Item
 
-
+# create Station object from GameObject that allows item to be contained in it
 class Station(GameObject):
     def __init__(self, item: Item = None):
         super().__init__()
         self.object_type = ObjectType.station
         self.item: Item = item
 
+    # item getter and setter methods
     @property
     def item(self) -> Item:
         return self.__item
@@ -18,9 +19,11 @@ class Station(GameObject):
     def item(self, item: Item):
         self.__item = item if isinstance(item, Item) else None
 
+    # take action method
     def take_action(self, avatar: avatar = None):
         return
 
+    # json methods
     def to_json(self) -> dict:
         dict_data = super().to_json()
         dict_data['item'] = self.item.to_json() if self.item else None
