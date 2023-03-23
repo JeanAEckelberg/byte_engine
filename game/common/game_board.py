35d813e6d8394b8a8377f298d44fe8451e00510d
KingPhilip14 <kingian404@gmail.com>
2023-03-23 14:14:44 -0500

    Finalized game_board
    Since lists can't be used as a key in a dict, it was changed to be a tuple. Both documentation and methods were adjusted for this.

--- game/common/game_board.py
+++ game/common/game_board.py
@@ -17,18 +17,29 @@ class GameBoard(GameObject):
         For example, a Vector object with an 'x' of 5 and a 'y' of 7 will create a board 5 tiles wide and
         7 tiles long
 
+        Example:
+            _ _ _ _ _  y = 0
+            |       |
+            |       |
+            |       |
+            |       |
+            |       |
+            |       |
+            _ _ _ _ _  y = 6
+
     -----------------------------------------------------------------------------------------------------------
 
     locations:
         This is the bulkiest part of the generation. The locations field is a dictionary with a key
-        being a list of Vectors, and the value being a list of GameObjects. This is used to assign the
-        given GameObjects the given coordinates via the Vectors. This is done in two ways:
+        being a tuple of Vectors, and the value being a list of GameObjects (the key is a tuple to prevent issues
+        with the generation system). This is used to assign the given GameObjects the given coordinates
+        via the Vectors. This is done in two ways:
 
             Statically:
                 If you want a GameObject to be at a specific coordinate, ensure that the key-value pair is
                 ONE Vector and ONE GameObject.
                 An example of this would be the following:
-                    locations = {[vector_2_4] : [station_0]}
+                    locations = {(vector_2_4) : [station_0]}
 
                 In this example, vector_2_4 contains the coordinates (2, 4). (Note that this naming convention
                 isn't necessary, but was used to help with the concept). Furthermore, station_0 is the
@@ -36,29 +47,31 @@ class GameBoard(GameObject):
 
             Dynamically:
                 If you want to assign multiple GameObjects to different coordinates, use a key-value
-                pair of any length. However, the assignments will be random.
-                An example of this would be the following:
+                pair of any length. NOTE: The length of the tuple and list MUST be equal, otherwise it will not work.
+                In this case, the assignments will be random. An example of this would be the following:
                     locations =
                         {
-                            [vector_0_0, vector_1_1, vector_2_2] : [station_0, station_1, station_2]
+                            (vector_0_0, vector_1_1, vector_2_2) : [station_0, station_1, station_2]
                         }
 
+                (Note that the tuple and list both have a length of 3).
                 When this is passed in, the three different vectors containing coordinates (0, 0), (1, 1), or
                 (2, 2) will be randomly assigned station_0, station_1, or station_2.
 
                 Therefore:
-                If station_0 is at (1, 1),
+                If station_0 is randomly assigned at (1, 1),
                 station_1 could be at (2, 2),
                 then station_2 will be at (0, 0).
+                This is just one case of what could happen.
 
         Lastly, another example will be shown to explain that you can combine both static and
         dynamic assignments in the same dictionary:
 
             locations =
                 {
-                    [vector_0_0] : [station_0],
-                    [vector_0_1] : [station_1],
-                    [vector_1_1, vector_1_2, vector_1_3] : [station_2, station_3, station_4]
+                    (vector_0_0) : [station_0],
+                    (vector_0_1) : [station_1],
+                    (vector_1_1, vector_1_2, vector_1_3) : [station_2, station_3, station_4]
                 }
 
         In this example, station_0 will be at vector_0_0 without interference. The same applies to
@@ -86,7 +99,7 @@ class GameBoard(GameObject):
     """
 
     def __init__(self, seed: int | None = None, map_size: Vector = Vector(),
-                 locations: dict[[Vector]:[GameObject]] | None = None, walled: bool = False):
+                 locations: dict[tuple[Vector]:list[GameObject]] | None = None, walled: bool = False):
 
         super().__init__()
         self.seed = seed
@@ -125,10 +138,14 @@ class GameBoard(GameObject):
         return self.__locations
 
     @locations.setter
-    def locations(self, locations: dict[[Vector]:[GameObject]] | None):
+    def locations(self, locations: dict[tuple[Vector]:list[GameObject]] | None):
         if locations is not None or not isinstance(locations, dict):
-            raise ValueError("Locations must be a dict. The key must be a list of Vector Objects, and the "
+            raise ValueError("Locations must be a dict. The key must be a tuple of Vector Objects, and the "
                              "value a list of GameObject.")
+        for k,v in locations:
+            if len(k) != len(v):
+                raise ValueError("Cannot set the locations for the game_board. A key has a different "
+                                 "length than its key.")
 
         self.__locations = locations
 
@@ -162,6 +179,8 @@ class GameBoard(GameObject):
             if len(k) != len(v) or (len(k) == 0 or len(v) == 0):  # Key-Value lengths must be > 0 and equal
                 raise ValueError("A key-value pair from game_board.locations has mismatching lengths. "
                                  "They must be the same length, regardless of size.")
+
+            # random.choices returns a randomized list which is used in __help_populate()
             j = random.choices(k, k=len(k))
             self.__help_populate(j, v)
 
@@ -224,7 +243,7 @@ class GameBoard(GameObject):
         self.game_map: list[list[GameObject]] = list((map(lambda tile: Tile().from_json(tile), y)) for y in temp)
         self.seed: int | None = data["seed"]
         self.map_size: Vector = data["map_size"]
-        self.locations: dict[[Vector]:[GameObject]] = data["locations"]
+        self.locations: dict[tuple[Vector]:list[GameObject]] = data["locations"]
         self.walled: bool = data["walled"]
         self.event_active = data['event_active']
         return self
