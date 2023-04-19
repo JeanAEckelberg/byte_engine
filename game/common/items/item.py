83f68d94591abb71dfb324f4f288257b5b2e414a
KingPhilip14 <kingian404@gmail.com>
2023-04-19 02:07:49 -0500

    Item.py documentation
    Started the documentation for item.py. Will have to ask questions on durability and stack_size to ensure it's documented correctly.

--- game/common/items/item.py
+++ game/common/items/item.py
@@ -4,14 +4,59 @@ from typing import Self
 
 
 class Item(GameObject):
+    """
+    Items have 4 different attributes: value, durability, quantity, stack-size.
+
+    Value:
+        The value of an item can be anything depending on the purpose of the game. For example, if a player can
+        sell an item to a shop for gold/money, the value attribute would be used. However, this value may be used
+        for anything to better fit the purpose of the game being created.
+
+    ----------------------------------------------------------------------------------------------------------------
+
+    Durability:
+        The value of an item's durability can be either None or an integer.
+
+        If durability is an integer, it allows for flexibility with the item. For example, if the durability is
+        0, the item could disappear, "break," etc.
+
+        If the durability is equal to None, it represents the item having infinite durability. In the case
+        the game being created needs items without durability, set this attribute to None to prevent any difficulties.
+
+    ----------------------------------------------------------------------------------------------------------------
+
+    Quantity and Stack Size:
+        These two work in tandem. Fractions (x/y) will be used to better explain the concept.
+
+        Quantity simply refers to the amount of the current item the player has. For example, having 5 gold, 10
+        gold, or 0 gold all work for representing the quantity.
+
+        The stack_size represents how *much* of an item can be in a stack. Think of this like the Minecraft inventory
+        system. For example, you can have 64 blocks of dirt, and if you were to gain one more, you would have a new
+        stack starting at 1.
+
+        In the case of items here, it works with the avatar.py file's inventory system (refer to those notes on how
+        the inventory works in depth). The Minecraft analogy should help understand the concept.
+
+        To better show this, quantity and stack size work like the following fraction model: quantity/stack_size.
+        The quantity can never be 0 and must always be a minimum of 1. Furthermore, quantity cannot be greater than
+        the stack_size. So 65/64 will not be possible.
+
+    ----------------------------------------------------------------------------------------------------------------
+
+    pick_up Method:
+
+    
+    """
+
     def __init__(self, value: int = 1, durability: int | None = 100, quantity: int = 1, stack_size: int = 1):
         super().__init__()
         self.__quantity = None  # This is here to prevent an error
-        self.__durability = None
+        self.__durability = None  # This is here to prevent an error
         self.object_type: ObjectType = ObjectType.ITEM
         self.value: int = value  # Value can more specified based on purpose (e.g., the sell price)
         self.stack_size: int = stack_size  # the max quantity this item can contain
-        self.durability: int | None = durability  # durability can be None if infinite durability
+        self.durability: int | None = durability  # durability can be None to represent infinite durability
         self.quantity: int = quantity  # the current amount of this item
 
     @property
@@ -35,7 +80,8 @@ class Item(GameObject):
         if durability is not None and not isinstance(durability, int):
             raise ValueError(f'{self.__class__.__name__}.durability must be an int or None.')
         if durability is not None and self.stack_size != 1:
-            raise ValueError(f'{self.__class__.__name__}.durability must be set to None if stack_size is not equal to 1.')
+            raise ValueError(
+                f'{self.__class__.__name__}.durability must be set to None if stack_size is not equal to 1.')
         self.__durability = durability
 
     @value.setter
