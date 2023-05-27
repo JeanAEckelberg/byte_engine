f4cb4aa472cd172f7338dd3adf63efda0053d53d
Gunnar Moody <115500822+MoodyMan04@users.noreply.github.com>
2023-05-26 22:53:12 -0500

    text class created
    created class for making text in rect
    Default Font: Bauhaus93
    Default Color: #daa520
    Default Position: Vector(0, 0)

--- Visualiser2/utils/text.py
+++ Visualiser2/utils/text.py
@@ -1,4 +1,13 @@
-# TODO: Create a basic text class with baked in rect
+import pygame
+from game.utils.vector import Vector
+
 class Text:
-    def __init__(self):
-        pass
+    def __init__(self, screen: pygame.Surface, text: str, font_size: int, font: str = 'bauhaus93', color: str = pygame.Color('#daa520'), position: Vector = Vector(0, 0)):
+        self.screen = screen
+        self.font = pygame.font.SysFont(font, font_size)
+        self.text_surface = self.font.render(text, True, color)
+        self.rect = self.text_surface.get_rect()
+        self.rect.topleft = position.as_tuple()
+
+    def render(self) -> None:
+        self.screen.blit(self.text_surface, self.rect)
