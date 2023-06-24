f062f81ca8aab774d3f25db4e3c2717bb5b5d70e
JeanAEckelberg <fyrelynx11@gmail.com>
2023-06-23 22:45:27 -0500

    Fixed it all... hopefully

--- Visualiser2/bytesprites/bytesprite.py
+++ Visualiser2/bytesprites/bytesprite.py
@@ -13,11 +13,12 @@ class ByteSprite(pyg.sprite.Sprite):
     layer: int
     rect: pyg.Rect
     screen: pyg.Surface
+    image: pyg.Surface
     __frame_index: int  # Selects the sprite from the spritesheet to be used. Used for animation
     __config: Config = Config()
 
-    def __init__(self, screen: pyg.Surface, filename: str, num_of_states: int, colorkey: pyg.Color | None,
-                 object_type: int, layer: int, top_left: Vector = Vector(0, 0)):
+    def __init__(self, screen: pyg.Surface, filename: str, num_of_states: int, colorkey: pyg.Color | None = None,
+                 object_type: int = 0, layer: int = 0, top_left: Vector = Vector(0, 0)):
         # Add implementation here for selecting the sprite sheet to use
         super().__init__()
         self.spritesheet_parser: SpriteSheet = SpriteSheet(filename)
@@ -108,13 +109,16 @@ class ByteSprite(pyg.sprite.Sprite):
             raise ValueError(f'{self.__class__.__name__}.screen must be a pyg.Screen object.')
         self.__screen = screen
 
+    # Inherit this method to implement sprite logic
     def update(self, data: dict, layer: int, pos: Vector) -> None:
+        super().update()
         self.__frame_index = 0  # Starts the new spritesheet at the beginning
-
-    def render(self, layer: int, pos: Vector) -> None:
-
-        self.__frame_index = self.__frame_index % self.__config.NUMBER_OF_FRAMES_PER_TURN
         self.rect.topleft = (
             pos.x * self.__config.TILE_SIZE * self.__config.SCALE + self.__config.GAME_BOARD_MARGIN_LEFT,
             pos.y * self.__config.TILE_SIZE * self.__config.SCALE + self.__config.GAME_BOARD_MARGIN_TOP)
 
+    # Call this method at the end of the implemented logic and for each frame
+    def set_image_and_render(self):
+        self.image = self.active_sheet[self.__frame_index]
+        self.__frame_index = (self.__frame_index + 1) % self.__config.NUMBER_OF_FRAMES_PER_TURN
+        self.screen.blit(self.image, self.rect)
