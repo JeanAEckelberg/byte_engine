b34bc19c7216b99ed890499e25c4ea3fa2ff948a
JeanAEckelberg <fyrelynx11@gmail.com>
2023-06-25 22:43:47 -0500

    Added functions to calculate logic and a data structure to store it all

--- Visualiser2/main.py
+++ Visualiser2/main.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from Visualiser2.adapter import Adapter
+from Visualiser2.bytesprites.bytesprite import ByteSprite
 from Visualiser2.config import Config
 from Visualiser2.utils.log_reader import logs_to_dict
 from game.utils.vector import Vector
@@ -25,11 +26,12 @@ class ByteVisualiser:
         self.simple_font = pygame.font.Font(None, 50)
 
         self.tick: int = 0
-        self.bytesprites = pygame.sprite.Group()
+        self.bytesprite_templates = pygame.sprite.Group()
+        self.bytesprite_map: [[[ByteSprite]]] = list()
 
     def load(self):
         self.turn_logs = logs_to_dict()
-        self.bytesprites = self.adapter.populate_bytesprites()
+        self.bytesprite_templates = self.adapter.populate_bytesprites()
 
     def prerender(self):
         self.screen.fill(self.config.BACKGROUND_COLOR)
@@ -38,15 +40,54 @@ class ByteVisualiser:
     def render(self):
         if self.tick % self.config.NUMBER_OF_FRAMES_PER_TURN == 0:
             # NEXT TURN
+            self.recalc_animation(self.turn_logs[f'turn_{self.tick // self.config.NUMBER_OF_FRAMES_PER_TURN + 1:04d}'])
             self.adapter.recalc_animation(
                 self.turn_logs[f'turn_{self.tick // self.config.NUMBER_OF_FRAMES_PER_TURN + 1:04d}'])
         else:
             # NEXT ANIMATION FRAME
+            self.continue_animation()
             self.adapter.continue_animation()
 
         self.adapter.render()
         pygame.display.flip()
         self.tick += 1
+
+    def recalc_animation(self, turn_data: dict) -> None:
+        game_map: [[dict]] = turn_data['game_board']['game_map']
+        row: list
+        for y, row in enumerate(game_map):
+            tile: dict
+            if len(self.bytesprite_map) < y + 1:
+                self.bytesprite_map.append(list())
+            for x, tile in enumerate(row):
+                if len(self.bytesprite_map[y]) < x + 1:
+                    self.bytesprite_map.append(list())
+                temp_tile: dict | None = tile
+                z: int = 0
+                while temp_tile is not None:
+                    if len(self.bytesprite_map[y][x]) < z + 1:
+                        self.bytesprite_map[y][x].append(None)
+
+                    if self.bytesprite_map[y][x][z] is None or self.bytesprite_map[y][x][z].object_type == temp_tile[
+                        'object_type']:
+                        sprite_class: ByteSprite | None = next(t for t in self.bytesprite_templates.sprites() if
+                                                               isinstance(t, ByteSprite) and t.object_type == temp_tile[
+                                                                   'object_type'])
+                        if sprite_class is None:
+                            raise ValueError(
+                                f'Must provide a bytesprite for each object type! Missing object_type: {temp_tile["object_type"]}')
+
+                        self.bytesprite_map[y][x][z] = sprite_class.__class__(self.screen)
+
+                    self.bytesprite_map[y][x][z].update(temp_tile, z, Vector(y=y, x=x))
+                    temp_tile = temp_tile.get('occupied_by')
+                    z += 1
+
+    def continue_animation(self) -> None:
+        row: list
+        tile: list
+        sprite: ByteSprite
+        [[[sprite.set_image_and_render() for sprite in tile] for tile in row] for row in self.bytesprite_map]
 
     def postrender(self):
         self.adapter.clean_up()
