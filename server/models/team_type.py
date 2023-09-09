6538cf6e348f4cc87c9bcc08350a2e58064c04e8
KingPhilip14 <kingian404@gmail.com>
2023-09-09 00:23:43 -0500

    Added unique
    Added the unique constraint to team_type_name and uni_name.

--- server/models/team_type.py
+++ server/models/team_type.py
@@ -6,6 +6,6 @@ from sqlalchemy.orm import relationship, Mapped, mapped_column
 class TeamType(Base):
     __tablename__ = 'team_type'
     team_type_id: Mapped[int] = mapped_column(primary_key=True)
-    team_type_name: Mapped[str] = mapped_column(String(15), nullable=False,
+    team_type_name: Mapped[str] = mapped_column(String(15), nullable=False, unique=True
                                                 constraint=CheckConstraint("team_type_name != ''"))
     eligible: bool = mapped_column(Boolean(), nullable=False)
--- server/models/university.py
+++ server/models/university.py
@@ -6,4 +6,4 @@ from sqlalchemy.orm import relationship, Mapped, mapped_column
 class University(Base):
     __tablename__ = 'university'
     uni_id: Mapped[int] = mapped_column(primary_key=True)
-    uni_name: Mapped[str] = mapped_column(String(100), nullable=False, constraint=CheckConstraint("uni_name != ''"))
+    uni_name: Mapped[str] = mapped_column(String(100), nullable=False, unique=True, constraint=CheckConstraint("uni_name != ''"))
