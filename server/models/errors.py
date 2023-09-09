43575f89d429d6e9e602389b39280b850c637e10
KingPhilip14 <kingian404@gmail.com>
2023-09-09 01:18:10 -0500

    Added new files
    Added errors and turn table files.

--- server/models/errors.py
+++ server/models/errors.py
@@ -5,3 +5,9 @@ from sqlalchemy.orm import relationship, Mapped, mapped_column
 
 class Errors(Base):
     __tablename__: str = 'errors'
+    run_id: Mapped[int] = mapped_column(Integer(), ForeignKey("run.run_id"), primary_key=True)
+    submission_id: Mapped[int] = mapped_column(Integer(), ForeignKey("submission.submission_id"), primary_key=True)
+    error_txt: Mapped[String] = mapped_column(String(), nullable=True)
+
+
+
