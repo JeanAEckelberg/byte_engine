bbc308658763a6ef4eedd523f4a387c910512147
KingPhilip14 <kingian404@gmail.com>
2023-09-09 00:18:05 -0500

    Added nullables
    Added nullables to existing files

--- server/models/group_run.py
+++ server/models/group_run.py
@@ -6,11 +6,10 @@ from sqlalchemy.orm import relationship, Mapped, mapped_column
 class GroupRun(Base):
     # Datetimes are stored in UTC in ISO format
     __tablename__='group_run'
-    group_run_id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
-    start_run: Mapped[DateTime] = mapped_column(DateTime())
+    group_run_id: Mapped[int] = mapped_column(Integer(), primary_key=True)
+    start_run: Mapped[DateTime] = mapped_column(DateTime(), nullable=False)
     launcher_version: Mapped[str] = mapped_column(String(10), nullable=False)
-    runs_per_client: Mapped[int] = mapped_column()
-    is_finished: Mapped[bool] = mapped_column(default=False)
-    seed_id: Mapped[int] = mapped_column()
-    seed: Mapped[int] = mapped_column()
+    runs_per_client: Mapped[int] = mapped_column(Integer(), nullable=False)
+    is_finished: Mapped[bool] = mapped_column(Boolean(), default=False, nullable=False)
+    seed: Mapped[int] = mapped_column(Integer(), nullable=False)
 
--- server/models/team_type.py
+++ server/models/team_type.py
@@ -8,4 +8,4 @@ class TeamType(Base):
     team_type_id: Mapped[int] = mapped_column(primary_key=True)
     team_type_name: Mapped[str] = mapped_column(String(15), nullable=False,
                                                 constraint=CheckConstraint("team_type_name != ''"))
-    eligible: bool = mapped_column(Boolean())
+    eligible: bool = mapped_column(Boolean(), nullable=False)
