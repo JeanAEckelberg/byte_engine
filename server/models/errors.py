7ed453f2e55c47bef88e7161b06983d11acecff2
JuliaCaesar <jmmotzko@gmail.com>
2023-09-15 21:36:50 -0500

    fixed again

--- server/models/errors.py
+++ server/models/errors.py
@@ -6,10 +6,9 @@ from server.models.base import Base
 
 class Errors(Base):
     __tablename__: str = 'errors'
-    run_id: Mapped[int] = mapped_column(Integer(), primary_key=True)  # run id pk
-    run_id_fk: Mapped[int] = mapped_column(Integer(), ForeignKey("run.run_id"))  # run id
-    submission_id: Mapped[int] = mapped_column(Integer(), primary_key=True)  # submission id
-    submission_id_fk: Mapped[int] = mapped_column(Integer(), ForeignKey("submission.submission_id"))  # submission id fk
+    error_id: Mapped[int] = mapped_column(Integer(), primary_key=True, autoincrement=True)  # run id pk
+    run_id: Mapped[int] = mapped_column(Integer(), ForeignKey("run.run_id"))  # run id
+    submission_id: Mapped[int] = mapped_column(Integer(), ForeignKey("submission.submission_id"))  # submission id fk
     error_txt: Mapped[str] = mapped_column(String(), nullable=True)
 
 
