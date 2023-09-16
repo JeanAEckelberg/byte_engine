e1185184671bff7823af31dcb4d068015b6b778f
JuliaCaesar <jmmotzko@gmail.com>
2023-09-15 21:05:36 -0500

    fixed?

--- server/models/turn_table.py
+++ server/models/turn_table.py
@@ -7,7 +7,8 @@ from server.models.base import Base
 class TurnTable(Base):
     __tablename__: str = 'turn_table'
     turn_number: Mapped[int] = mapped_column(Integer(), primary_key=True)
-    run_id: Mapped[int] = mapped_column(Integer(), ForeignKey('run.run_id'), primary_key=True)
+    run_id: Mapped[int] = mapped_column(Integer(), primary_key=True)
+    run_id_fk: Mapped[int] = mapped_column(Integer(), ForeignKey('run.run_id'))
     turn_data: Mapped[str] = mapped_column(LargeBinary(), nullable=False)
 
 
