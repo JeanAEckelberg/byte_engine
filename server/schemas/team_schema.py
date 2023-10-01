766e9f29bf1048002b514e45341acb547951e7dc
KingPhilip14 <kingian404@gmail.com>
2023-10-01 15:22:56 -0500

    Fixed private issue
    Made team_id_uuid its own schema for privacy.

--- server/schemas/team_schema.py
+++ server/schemas/team_schema.py
@@ -4,13 +4,16 @@ import university_schema, team_type_schema, submission_schema
 
 
 class TeamBase(BaseModel):
-    team_id_uuid: int
     uni_id: int
     team_type_id: int
     team_name: str
 
     class Config:
         from_attributes = True
+
+
+class TeamIdSchema(TeamBase):
+    team_id_uuid: int
 
 
 # University <-> Team: Many to One
