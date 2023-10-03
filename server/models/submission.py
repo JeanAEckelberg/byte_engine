27f246ac46a3fccb171e74777100e63a5e5870d3
Jean A. Eckelberg <fyrelynx11@gmail.com>
2023-10-03 14:13:20 -0500

    Updated models to include necessary relationships and created CRUD operations for the submission table (#102)

--- server/models/submission.py
+++ server/models/submission.py
@@ -1,7 +1,11 @@
+from __future__ import annotations
+
 from sqlalchemy import LargeBinary, ForeignKey, Integer, DateTime
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
+from .errors import Errors
+from .team import Team
 
 
 class Submission(Base):
@@ -10,3 +14,6 @@ class Submission(Base):
     team_id_uuid: Mapped[int] = mapped_column(Integer(), ForeignKey("team.team_id_uuid"))
     submission_time: Mapped[str] = mapped_column(DateTime(), nullable=False)
     file_txt: Mapped[str] = mapped_column(LargeBinary(), nullable=False)
+
+    team: Mapped[Team] = relationship(back_populates="submissions")
+    error: Mapped[Errors] = relationship(back_populates='submission')
