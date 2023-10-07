cae7ef6695f323fcfc93fa2d184bf747bfc227b9
KingPhilip14 <kingian404@gmail.com>
2023-10-07 00:00:04 -0500

    Error api calls
    Made api calls that will return a list of errors based on the  given submission. The submission is accessed by the id and uuid.

--- server/main.py
+++ server/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException, Depends
 from sqlalchemy.orm import Session
 from models.base import Base
 from database import SessionLocal, engine
+from crud import crud_errors, crud_submission
 from models.run import Run
 from models.errors import Errors
 from models.group_run import GroupRun
@@ -10,6 +11,9 @@ from models.team_type import TeamType
 from models.submission import Submission
 from models.turn_table import TurnTable
 from models.university import University
+from schemas.university_schema import *
+from schemas.submission_schema import *
+from schemas.errors_schema import *
 
 Base.metadata.create_all(bind=engine)
 
@@ -26,6 +30,24 @@ def get_db():
 
 # API
 
-@app.get("/")
+@app.get('/')
 def root():
     return {"message": "Hello World"}
+
+
+@app.get('/get_errors_for_submission', response_model=list[ErrorsBase])
+def get_errors_for_submission(submission: SubmissionWTeam, db: Session = Depends(get_db)):
+    # Retrieves a list of submissions where the submission id and uuids match
+    submission_list: list[Submission] | None = crud_submission.read_all_W_filter(
+        db, submission_id=submission.submission_id, team_id_uuid=submission.team_id_uuid)
+
+    if submission_list is None:
+        raise HTTPException(status_code=404, detail="Submission not found!")
+
+    return submission_list[0].errors
+
+@app.get('/')
+
+
+# @app.get('/unis/', response_model=list[UniversityBase])
+# def get_unis():
