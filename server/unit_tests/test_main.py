9a575fcf17496050e936950d6b534c8a4c88d412
JosiahErnst <144740919+JosiahErnst@users.noreply.github.com>
2023-10-20 23:10:14 -0500

    Updated main.py testing (#120)
    * Updated main.py testing
    
    Added the new test for reading by submissions.
    
    * Removing print statements
    
    Just removed print statements for unit testing

--- server/unit_tests/test_main.py
+++ server/unit_tests/test_main.py
@@ -12,7 +12,6 @@ def test_read_root():
 
 def test_read_get_submission():
     response = client.get('/get_submission/1/1/')
-    print(response.json())
     assert response.json() == {"submission_id": 1,
          "submission_time": "2000-10-31T01:30:00-05:00",
          "file_txt": "test",
@@ -27,3 +26,21 @@ def test_read_get_submission():
                                            "group_run_id": 1,
                                            "run_time": "2000-10-31T01:30:00-05:00",
                                            "seed": 1}}]}
+
+
+def test_read_get_submissions():
+    response = client.get('/get_submissions/1')
+    assert response.json() == [{"submission_id": 1,
+        "submission_time": "2000-10-31T01:30:00-05:00",
+        "file_txt": "test",
+        "team": {"uni_id": 1,
+                "team_type_id": 1,
+                "team_name": "Noobs"},
+        "submission_run_infos": [{"submission_run_info_id": 1,
+                                    "run_id": 1,
+                                    "submission_id": 1,
+                                    "error_txt": "error",
+                                    "run": {"run_id": 1,
+                                            "group_run_id": 1,
+                                            "run_time": "2000-10-31T01:30:00-05:00",
+                                            "seed": 1}}]},]
