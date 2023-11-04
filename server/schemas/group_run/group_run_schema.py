e51645f0a989df7ff46b1ee81cd0c12c4dd650bd
JuliaCaesar <jmmotzko@gmail.com>
2023-11-03 23:33:19 -0500

    changed to run schema without group run

--- server/schemas/group_run/group_run_schema.py
+++ server/schemas/group_run/group_run_schema.py
@@ -1,9 +1,8 @@
 from server.schemas.group_run.group_run_base import GroupRunBase
-from server.schemas.group_teams.group_teams_base import GroupTeamsBase
 from server.schemas.group_teams.group_teams_w_team import GroupTeamsWTeam
-from server.schemas.run.run_base import RunBase
+from server.schemas.run.run_schema_wo_group_run import RunSchemaWithoutGroupRun
 
 
 class GroupRunSchema(GroupRunBase):
-    runs: list[RunBase]
+    runs: list[RunSchemaWithoutGroupRun]
     group_teams: list[GroupTeamsWTeam]
