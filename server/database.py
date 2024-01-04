96369d27e713e0fddcf886c41b2138fada1fb31d
Jean A. Eckelberg <fyrelynx11@gmail.com>
2024-01-03 22:50:29 -0600

    migrated back to sqlite

--- server/database.py
+++ server/database.py
@@ -1,10 +1,19 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
+from sqlalchemy.engine import Engine
 from sqlalchemy.orm import sessionmaker
 
-DB_URL = 'postgresql+psycopg2://byteuser:bytepassword@localhost:5432/byteserver'
+DB_URL = 'sqlite:///./byte_server.db'
 
 engine = create_engine(
-    DB_URL
+    DB_URL, connect_args={'check_same_thread': False}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.execute("PRAGMA journal_mode=WAL")
+    cursor.close()
