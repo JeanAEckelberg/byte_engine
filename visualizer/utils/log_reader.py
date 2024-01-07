035a546721102464da8068add8f0ebccc17cd378
KingPhilip14 <kingian404@gmail.com>
2024-01-07 16:27:27 -0600

    Update log_reader.py
    Added a doc string

--- visualizer/utils/log_reader.py
+++ visualizer/utils/log_reader.py
@@ -6,6 +6,11 @@ import json
 
 # Takes the logs and puts them in a dictionary
 def logs_to_dict(log_dir: str | None) -> dict:
+    """
+    Takes the given log directory and and puts them into a dictionary.
+    :param log_dir:
+    :return: dict
+    """
     temp: dict = {}
     for file in Path(gc.LOGS_DIR if log_dir is None else log_dir).glob('*.json'):
         with open(file, 'r') as f:
