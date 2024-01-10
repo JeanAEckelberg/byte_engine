62c87c6e9d9d6ebf524dcc3f393a38c67f623596
Jean A. Eckelberg <fyrelynx11@gmail.com>
2024-01-10 17:37:56 -0600

    Added documentation to visualizer config

--- visualizer/config.py
+++ visualizer/config.py
@@ -2,6 +2,66 @@ from game.utils.vector import Vector
 
 
 class Config:
+    """
+    Config
+    ------
+        Configuration class for the game visualizer. By encapsulating the visualizer settings in a class, the readonly
+        nature of the following settings are enforced to a greater degree.
+
+    Number Of Frames Per Turn
+    +++++++++++++++++++++++++
+        Number of frames per turn is an int representing the number of animation frames on
+        each turn. See spritesheets for an example of what is needed for 4 frames per turn.
+        Set this to 1 if you do not want any animations.
+    -----
+
+    Tile Size
+    +++++++++
+        Tile size is an int that represents the size of each sprite (it's a square, so this
+        applies to both width and height).
+    -----
+
+    Scale
+    +++++
+        Scale is an int that represents the ratio between the tile size and the number of
+        pixels that it will take on the visualizer itself.
+    -----
+
+    Screen Size
+    +++++++++++
+        Screen size is a Vector that represents the dimensions of the visualizer.
+    -----
+
+    Frame Rate
+    ++++++++++
+        Frame rate is an int that represents the number of frames per second.
+    -----
+
+    Background Color
+    ++++++++++++++++
+        Background Color is a tuple of 3 ints representing the red, green, and blue values
+        of the background.
+    -----
+
+    Gameboard Margin Left
+    +++++++++++++++++++++
+        Gameboard Margin Left is an int representing the margin from the left side
+        of the screen that the gameboard will adhere to.
+    -----
+
+    Gameboard Margin Top
+    ++++++++++++++++++++
+        Gameboard Margin Top is an int representing the margin from the top side of
+        the screen that the gameboard will adhere to.
+    -----
+
+    Visualize Held Items
+    ++++++++++++++++++++
+        Visualize Held Items is a boolean that represents if held items should be
+        rendered and has supporting ByteSprites.
+    -----
+    """
+
     __NUMBER_OF_FRAMES_PER_TURN: int = 4
     __TILE_SIZE: int = 16
     __SCALE: int = 5
@@ -12,45 +72,67 @@ class Config:
     __GAME_BOARD_MARGIN_TOP: int = 100
     __VISUALIZE_HELD_ITEMS: bool = True
 
-    # if you have an animation, this will be the number of frames the animation goes through for each turn
     @property
     def NUMBER_OF_FRAMES_PER_TURN(self) -> int:
+        """
+        If you have an animation, this will be the number of frames the animation goes through for each turn.
+        """
         return self.__NUMBER_OF_FRAMES_PER_TURN
 
-    # this will be the size of the tile-its going to be squares
     @property
     def TILE_SIZE(self) -> int:
+        """
+        This will be the size of the tile-its going to be squares
+        """
         return self.__TILE_SIZE
 
-    # scale is for the tile size being scaled larger,
-    # for ex: if you have a 16x16 tile, we can scale it to 4 so it looks larger
     @property
     def SCALE(self) -> int:
+        """
+        Scale is for the tile size being scaled larger.
+        For example: With a 16x16 tile, it can be scaled by 4 to be a 64x64 tile on the visualizer
+        """
         return self.__SCALE
 
-    # the screen size is the overall screen size
     @property
     def SCREEN_SIZE(self) -> Vector:
+        """
+        The screen size is the overall screen size
+        """
         return self.__SCREEN_SIZE
 
-    # frame rate is the overall frame rate
+    #
     @property
     def FRAME_RATE(self) -> int:
+        """
+        Frame Rate is the overall frames per second.
+        """
         return self.__FRAME_RATE
 
-    # this is where you can set the default background color
     @property
     def BACKGROUND_COLOR(self) -> (int, int, int):
+        """
+        This is where you can set the default background color.
+        """
         return self.__BACKGROUND_COLOR
 
     @property
     def GAME_BOARD_MARGIN_LEFT(self) -> int:
+        """
+        This is where you can set the left margin for the main game board.
+        """
         return self.__GAME_BOARD_MARGIN_LEFT
 
     @property
     def GAME_BOARD_MARGIN_TOP(self) -> int:
+        """
+        This is where you can set the top margin for the main game board.
+        """
         return self.__GAME_BOARD_MARGIN_TOP
 
     @property
     def VISUALIZE_HELD_ITEMS(self) -> bool:
+        """
+        This is where you can set if you want held items to be displayed in the visualizer.
+        """
         return self.__VISUALIZE_HELD_ITEMS
