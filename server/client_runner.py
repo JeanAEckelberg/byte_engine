641449c1319cadfc64afccee7fc46c26c492a077
KingPhilip14 <kingian404@gmail.com>
2024-01-15 01:04:57 -0600

    Method docs
    Documented methods that I could make sense of.

--- server/client_runner.py
+++ server/client_runner.py
@@ -45,14 +45,32 @@ class ClientRunner:
             are used by the client_runner and visualizer_runner. Use that file to change the values, and **DO NOT**
             change them in any other file.
 
-            self.total_number_of_games: An int representing the number of games to run during a tournament
+            self.total_number_of_games: An int representing the number of games to run during a tournament.
 
-            self.number_of
+            self.number_of_unique_games: An int representing the unique number of combinations of clients to match them
+            against each other.
 
-            self.index_to_seed_id
+            self.index_to_seed_id: Maps a seed index to a database seed_id.
+
+            self.job_queues: A list of Queue objects to handle computations in threads. This helps improve the
+            performance of running clients against each other. Without this, tournaments would be significantly slower.
+
+            self.version: The version number of the project.
+
+            self.best_run_for_client: A dict that contains the information of the best performance of a client.
+
+            self.runner_temp_dir: A string representing the directory path to place the runner_temp folder.
+
+            self.seed_path: A string representing the directory path used to place the ``seeds`` file in the runner_temp
+            folder.
+
+            self.total_number_of_games_for_one_client: An int representing the number of times a client will run against
+            the opponents.
+
+            self.tournament: This will either be an int to represent the tournament_id that can be accessed from the
+            database, or a Tournament object itself with all the information needed.
         """
         self.config: Config = Config()
-
 
         self.total_number_of_games: int = -1
 
@@ -76,8 +94,9 @@ class ClientRunner:
 
         self.tournament: int | Tournament = -1
 
-        # self.loop.run_in_executor(None, self.await_input)
-        # self.loop.call_later(5, self.external_runner())
+        # Based on the sleep time specified in ``server_config.py``, this will have the client runner run until the
+        # ending date and time of the competition. This code doesn't need to be changed, but the values in
+        # ``server_config.py`` will need to be. This also calls the ``external_runner() method``.
         (schedule.every(self.config.SLEEP_TIME_SECONDS_BETWEEN_RUNS)
          .seconds
          .until(self.config.END_DATETIME)
@@ -87,8 +106,10 @@ class ClientRunner:
          .at(self.config.END_DATETIME.split()[-1])
          .do(self.close_server))
 
+        # Creates the Univeristies and Team Types in the database
         self.buffUpData()
 
+        # An infinite loop to keep the schedule object running
         try:
             while 1:
                 schedule.run_pending()
@@ -101,6 +122,25 @@ class ClientRunner:
             self.close_server()
 
     def external_runner(self) -> None:
+        """
+        This method is extensive and has different steps to it. Print statements help show where the runner is in the
+        process.
+
+        #. Step 1: Get the most recent submission for every team that is in the database.
+        #. Step 2:
+            * a) If there are less than 2 total submissions, end the method; nothing can be done
+            * b) Otherwise, proceed to the next step
+        #. Step 3: Get all team pairings based on all clients in the database. This will be the pairings for each game.
+        #. Step 4: Assign a value to ``self.total_number_of_games_for_one_client`` based on what's returned from the
+        ``self.count_number_of_game_appearances()`` method.
+        #. Step 5: Insert a new tournament into the database and assign it to ``self.tournament``.
+        #. Step 6: Delete all turns from the database. Witihout doing so, the Turns table will continue to accumulate
+        data for each new tournament and become very cumbersome.
+        #. Step 7: Make the ``runner_temp`` and ``seeds`` folders
+        #. Step 8: For the amount of games against the same client specified in ``server_config.py``, run the runner.
+        #. Step 9: Run the games in parallel with each other to increase performance.
+        :return: None
+        """
         print('running')
         self.best_run_for_client = {}
         with DB() as db:
@@ -257,9 +297,9 @@ class ClientRunner:
 
     def insert_new_tournament(self) -> Tournament:
         """
-        Inserts a new tournament. Relates all the runs in this process together
+        Inserts a new Tournament in the database and returns it. Relates all the runs in this process together
+        :return: a Tournament object
         """
-
         with DB() as db:
             return crud_tournament.create(db, TournamentBase(tournament_id=0,
                                                              start_run=datetime.utcnow(),
@@ -269,7 +309,11 @@ class ClientRunner:
 
     def insert_run(self, tournament_id: int, seed_id: int, results: dict) -> int:
         """
-        Inserts a run into the DB
+        Inserts a new Run in the database and returns it.
+        :param tournament_id:
+        :param seed_id:
+        :param results:
+        :return: the newly inserted Run's id
         """
         with DB() as db:
             return crud_run.create(db, RunBase(run_id=0,
@@ -281,7 +325,7 @@ class ClientRunner:
     def insert_submission_run_info(self, submission_id: int, run_id: int, error: str | None, player_num: int,
                                    points_awarded: int) -> None:
         """
-
+        Inserts a new SubmissionRunInfo entry in the database.
         :param submission_id:
         :param run_id:
         :param error:
@@ -303,7 +347,7 @@ class ClientRunner:
 
     def delete_tournament_cascade(self, tournament_id: int) -> None:
         """
-        Deletes the tournament by using the given id
+        Deletes the specified Tournament from the database by using the given id.
         :param tournament_id:
         :return: None
         """
@@ -311,6 +355,10 @@ class ClientRunner:
             crud_tournament.delete(db, tournament_id)
 
     def read_best_logs_and_insert(self) -> None:
+        """
+        Finds the best logs from the client and inserts them into the database
+        :return: None
+        """
         for submission_id in self.best_run_for_client:
             path = self.best_run_for_client[submission_id]["log_path"]
             turn_logs: list[TurnBase] = []
@@ -325,7 +373,9 @@ class ClientRunner:
 
     def insert_logs(self, logs: list[TurnBase]) -> None:
         """
-        Inserts logs
+        Inserts the given logs in the database.
+        :param logs: a list of Turn objects to insert into the database
+        :return: None
         """
         try:
             with DB() as db:
@@ -336,6 +386,11 @@ class ClientRunner:
             ...
 
     def close_server(self) -> None:
+        """
+        This method will delete the tournament from the database if it exists and is not finished with all the games.
+        Regardless, the ``runner_temp`` folder will be deleted.
+        :return: None
+        """
         if self.tournament != -1 and not self.tournament.is_finished:
             self.delete_tournament_cascade(self.tournament.tournament_id)
         else:
@@ -345,6 +400,10 @@ class ClientRunner:
         sys.exit(0)
 
     def delete_runner_temp(self) -> None:
+        """
+        Continually tries to delete the ``runner_temp`` folder.
+        :return: None
+        """
         while True:
             try:
                 if os.path.exists(self.runner_temp_dir):
@@ -354,11 +413,21 @@ class ClientRunner:
                 continue
 
     def delete_turns(self) -> None:
+        """
+        Deletes all entries from the Turn datatable.
+        :return: None
+        """
         with DB() as db:
             crud_turn.delete_all(db)
 
     def return_team_parings(self, submissions: list[Submission]) -> list[tuple[Submission, Submission]]:
-        # do not remove
+        """
+        This method will take a list of Submission entries and a list with every pairing of them. Also considers
+        the number of games against the same team and increases the pairings by that value.
+        :param submissions:
+        :return: a list of tuples that has the submissions of each team pairing
+        """
+        # do not remove comment below:
         # noinspection PyTypeChecker
         fixtures: list[tuple[Submission, Submission]] = list(itertools.permutations(submissions, 2))
         
@@ -369,6 +438,11 @@ class ClientRunner:
         return repeated
 
     def count_number_of_game_appearances(self, games: list[tuple[Submission, Submission]]) -> int:
+        """
+        Returns the number of games a client appears in.
+        :param games:
+        :return: the count of games a client appears in
+        """
         one_id: int = games[0][0].submission_id
         count: int = sum([1 for game_tuple in games
                           if game_tuple[0].submission_id == one_id
@@ -377,8 +451,8 @@ class ClientRunner:
 
     def update_tournament_finished(self) -> None:
         """
-        Updates the tournament to have the finished bool be True
-        :return:
+        Updates the tournament to have the ``is_finished`` bool to be True.
+        :return: None
         """
         self.tournament.is_finished = True
         with DB() as db:
@@ -386,6 +460,11 @@ class ClientRunner:
                                                      TournamentBase(**self.tournament.__dict__))
 
     def buffUpData(self) -> None:
+        """
+        This will create the different universities and team types to add to the database. This can be modified for
+        different competitions; add or subtract from it as needed.
+        :return: None
+        """
         with DB() as db:
             try:
                 crud_university.create(db, UniversityBase(
